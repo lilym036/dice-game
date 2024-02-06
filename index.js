f2f5174b7cdd4ae4db4cb1b275e5496e5437c798
Lilibeth Mejia <mejialily036@gmail.com>
2024-02-06 16:10:36 -0600

    added DOM code

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const randomNumber1= Math.ceil(Math.random() * 6);
 console.log(randomNumber1);
+// randomImage= `dice${randomNumber1}.png`
+// console.log(randomImage);
+
+document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
+document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
