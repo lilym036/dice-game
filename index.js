05243ecda7028aac70206426c03a2902891af417
Lilibeth Mejia <mejialily036@gmail.com>
2024-02-06 16:11:12 -0600

    hardcoded images on dice

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ console.log(randomNumber1);
 // randomImage= `dice${randomNumber1}.png`
 // console.log(randomImage);
 
-document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
+document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
 document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
