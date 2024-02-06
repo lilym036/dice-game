5747d16461c00b954baf5f96aac60385d3ba7023
Lilibeth Mejia <mejialily036@gmail.com>
2024-02-06 16:18:34 -0600

    changed heading with javascript

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ console.log(randomNumber1);
 
 document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
 document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
+
+const img1= document.querySelector(".img1");
+const img2= document.querySelector(".img2");
+
+function winOrLoose() {
+if (img1 > img2) {
+    document.querySelector("h1").innerHTML= "Player 1 wins"
+} else if (img1 < img2) {
+    document.querySelector("h1").innerHTML= "Player 2 Wins!"
+} else {
+    document.querySelector("h1").innerHTML= "It's a tie!"
+}
+}
+
+winOrLoose();
