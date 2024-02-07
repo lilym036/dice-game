fdfbb00eec0f2cb70f63d0142d21c54546effdbc
Lilibeth Mejia <mejialily036@gmail.com>
2024-02-06 20:02:54 -0600

    made changes

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const randomNumber1= Math.ceil(Math.random() * 6);
-const randomNumber2= Math.ceil(Math.random() * 6);
-console.log(randomNumber1);
-console.log(randomNumber2);
-imgSrc1= `./images/dice${randomNumber1}.png`
-imgSrc2= `./images/dice${randomNumber2}.png`
+const img1 = document.querySelector(".img1");
+const img2 = document.querySelector(".img2");
+const button = document.querySelector(".btn");
 
-const img1= document.querySelector(".img1");
-const img2= document.querySelector(".img2");
+const winOrLoose = () => {
+    const randomNumber1 = Math.ceil(Math.random() * 6);
+    const randomNumber2 = Math.ceil(Math.random() * 6);
+    console.log(randomNumber1);
+    console.log(randomNumber2);
+    imgSrc1 = `./images/dice${randomNumber1}.png`;
+    imgSrc2 = `./images/dice${randomNumber2}.png`;
 
-img1.setAttribute("src", imgSrc1);
-img2.setAttribute("src", imgSrc2);
+    img1.setAttribute("src", imgSrc1);
+    img2.setAttribute("src", imgSrc2);
 
-
-const winOrLoose= () => {
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML= "Player 1 wins 🎉"
-} else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML= "Player 2 Wins! 🎉"
-} else {
-    document.querySelector("h1").innerHTML= "It's a tie!"
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 wins 🎉"
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🎉"
+    } else {
+        document.querySelector("h1").innerHTML = "It's a tie!"
+    }
 }
-}
 
-winOrLoose();
+
+button.addEventListener("click", winOrLoose);
